6420e249dffa1e89833674529da0747e257d5aa6
Tayrnarion <anto9garnier@gmail.com>
2023-09-19 15:57:27 +0200

    Added 2-way databinding, changed the create server and added username field and reset

--- src/app/servers/servers.component.ts
+++ src/app/servers/servers.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-servers',
@@ -6,5 +6,40 @@ import { Component } from '@angular/core';
   styleUrls: ['./servers.component.css']
 })
 export class ServersComponent {
+  allowNewServer = false;
+  isEmpty = true;
+  serverCreationStatus = "No server was created";
+  serverName = "TestServer";
+  userName = "";
 
+  constructor () {
+    setTimeout(() => {
+      this.allowNewServer = true;
+    }, 2000);
+   }
+
+  ngOnInit() {
+
+  }
+
+  onCreateServer() {
+    this.serverCreationStatus = "Server was created! Name is " + this.serverName;
+  }
+
+  onUpdateServerName(event: any) {
+    this.serverName = (<HTMLInputElement>event.target).value;
+  }
+
+  onUpdateUserName(event: any) {
+    if (this.userName != "") {
+      this.isEmpty = false;
+    } else {
+      this.isEmpty = true;
+    }
+  }
+
+  onReset() {
+    this.userName = "";
+    this.isEmpty = true;
+  }
 }
